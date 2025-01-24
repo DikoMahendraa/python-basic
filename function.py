5d3c6132449e6925b57c535e8a7affc59a7c2a32
diko-mahendra <diko.dev99@gmail.com>
2025-01-25 00:55:47 +0700

    create a functionn

---
+++ function.py
@@ -0,0 +1,21 @@
+# To create a function in Python, you use the def keyword followed by the function name, parentheses (), and a colon :.
+
+
+def function_name(parameters):
+    # Function body (code to execute)
+    return parameters  # Optional: return a value
+
+def function_additional(a, b):
+    return a + b
+
+print(function_name("Hello World"))
+print(function_additional(2, 4))
+
+# Key Points:
+# Use def to define a function.
+
+# Use return to send a value back from the function (optional).
+
+# Functions can take parameters and return values.
+
+# You can call a function by using its name followed by parentheses ().
