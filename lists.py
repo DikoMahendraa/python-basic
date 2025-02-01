821439dd34db354c23343e4ca791587b5f87aa39
diko-mahendra <diko.dev99@gmail.com>
2025-02-01 21:34:20 +0700

    list

---
+++ lists.py
@@ -0,0 +1,16 @@
+# cars = ["bmw", "honda", "porsche", "toyota", "nissan", "mitshubishi"]
+
+# print(cars) #["bmw", "honda", "porsche", "toyota", "nissan", "mitshubishi"]
+# print(cars[2:5]) #['porsche', 'toyota', 'nissan']
+
+
+# count list items len() or count character (is like .length if on javascript)
+# print(len(cars))
+
+
+# added data to list
+foods = ["sate", "nasgor", "sayur"]
+foods.append("bubur") #added or put data to foods as the last index
+
+foods[1] = "kentang bakar" #change value foods index [1] to kentang bakar
+print(foods)
